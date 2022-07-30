17dc7d1b0bc1b9daede1420dca1028f534ef5373
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-30 01:06:00 -0300

    ✨ Created acessbile keys to up and down values(#7)

--- src/components/Fields/InputCurrency/index.tsx
+++ src/components/Fields/InputCurrency/index.tsx
@@ -1,29 +1,44 @@
 import { CSS } from '@stitches/react';
-import React from 'react';
+import React, { useCallback } from 'react';
 import CurrencyInput, { CurrencyInputProps } from 'react-currency-input-field';
 import { useIntl } from 'react-intl';
 import { Container, CurrencyInputStyled, Label } from './styles';
 
 
-type InputCurrencyType = React.PropsWithoutRef<CurrencyInputProps>  & {
+type InputCurrencyType = React.PropsWithoutRef<CurrencyInputProps> & {
   idLabel?: string;
   cssContainer?: CSS
 }
 
-const InputCurrency: React.FC<InputCurrencyType> = ({ idLabel, cssContainer, children, ...props }) => {
-  const { locale, formatMessage } = useIntl()
-  
+const InputCurrency: React.FC<InputCurrencyType> = (
+  { name, value, disabled, onValueChange, children, ...props }
+) => {
+  const { locale, formatMessage } = useIntl();
+
+  const handleArrowKeys = useCallback((e?: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e && onValueChange) {
+      if ("ArrowUp" === e.key) {
+        console.log(Number(value))
+        onValueChange((Number(value) + 1).toString(), name);
+        
+      } else if ("ArrowDown" === e.key) {
+        onValueChange((Number(value) - 1).toString(), name)
+      }
+    }
+  }, [value])
+
   return (
-    <Container css={cssContainer}>
-      {idLabel &&
-        <Label>{formatMessage({ id: idLabel })}</Label>
+    <Container css={props.cssContainer}>
+      {props.idLabel &&
+        <Label>{formatMessage({ id: props.idLabel })}</Label>
       }
       <CurrencyInputStyled
         intlConfig={{ locale, currency: 'BRL' }}
-        name={props.name}
-        value={props.value}
-        disabled={props.disabled}
-        onValueChange={props.onValueChange}
+        name={name}
+        value={value}
+        disabled={disabled}
+        onValueChange={onValueChange}
+        onKeyDown={handleArrowKeys}
       />
       {children}
     </Container>
