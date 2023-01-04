d1f895b5f713ace7f4650b8da294209768653cd9
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2023-01-04 00:36:07 -0300

    [🧪] User Component Test

---
+++ src/components/User/User.test.tsx
@@ -0,0 +1,27 @@
+import { describe, expect, test } from "vitest";
+import { render } from "../../__tests__/utils/test-utils";
+import User from ".";
+import "happy-dom"
+
+const setup = () => {
+  const renderResult = render(<User />)
+  return {
+    ...renderResult,
+    userImage: renderResult.getByAltText(/user image/i) as HTMLImageElement
+  }
+}
+
+describe("User Tests", () => {
+  test("Displays default image", () => {
+    const { userImage } = setup();
+
+    expect(userImage.src).toContain("defaultUserImage.jpg")
+  })
+
+  test("Displays user name", async () => {
+    const { user: { name }, findByText } = setup();
+
+    const userName = await findByText(name)
+    expect(userName).toBeDefined()
+  })
+})
