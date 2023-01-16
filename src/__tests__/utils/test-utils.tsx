4ce8c4b3ffa1cd1708fa85da44d5d09b6bd92862
Ítalo Médici <italo.medici@touchcomp.b>
2023-01-15 22:55:24 -0300

    Improve test utils

--- src/__tests__/utils/test-utils.tsx
+++ src/__tests__/utils/test-utils.tsx
@@ -1,52 +1,75 @@
 import React from "react";
-import { RenderOptions, render as tRender } from "@testing-library/react";
+import {
+  RenderOptions,
+  render as tRender,
+  renderHook as tRenderHook,
+} from "@testing-library/react";
 import { IntlProvider } from "react-intl";
 import { messages } from "../../i18n";
-import { BrowserRouter } from "react-router-dom";
 import ToastProvider from "../../contexts/toast";
 import { AuthProvider } from "../../contexts/auth";
 import { buildUserTest } from "./buildUserTest";
 import { User } from "../../models/User";
-
+import { BrowserRouter } from "react-router-dom";
+import "happy-dom"
+import "@testing-library/jest-dom"
 
 type renderOptions = Partial<{
   locale: string;
-  route: string,
-  userProps: Partial<User>,
-}> & RenderOptions
+  route: string;
+  userProps: Partial<User>;
+}> &
+  RenderOptions;
+
+function buildWrapper(
+  ui: React.ReactElement,
+  locale: string,
+  userMock: User | undefined,
+  route: string
+) {
+  window.history.pushState({}, "Test page", route);
+
+  return (
+    <BrowserRouter>
+      <IntlProvider locale={locale} messages={messages}>
+        <ToastProvider>
+          <AuthProvider userMock={userMock}>{ui}</AuthProvider>
+        </ToastProvider>
+      </IntlProvider>
+    </BrowserRouter>
+  );
+}
 
 function render(
   ui: React.ReactElement,
-  { 
-    locale = "BR", 
-    route = "/", 
+  {
+    locale = "BR",
+    route = "/",
     userProps,
     ...renderOptions
   }: renderOptions = {}
 ) {
-  window.history.pushState({}, "Test page", route);
   const userMock = buildUserTest(userProps);
 
-  function build() {
-    return (
-      <IntlProvider locale={locale} messages={messages}>
-        <ToastProvider>
-          <AuthProvider userMock={userMock}>
-            {ui}
-          </AuthProvider>
-        </ToastProvider>
-      </IntlProvider>
-    );
-  }
-
   return {
-    ...tRender(build(), { wrapper: BrowserRouter, ...renderOptions}),
-    user: userMock
+    ...tRender(buildWrapper(ui, locale, userMock, route), { ...renderOptions }),
+    userMock,
   };
+}
+
+function renderHook(
+  hook: (props?: any) => any,
+  { locale = "BR", route = "/", userProps }: renderOptions = {}
+) {
+  const userMock = buildUserTest(userProps);
+
+  return tRenderHook(hook, {
+    wrapper: ({ children }) => buildWrapper(children, locale, userMock, route),
+  });
 }
 
 // re-export everything
 export * from "@testing-library/react";
 
 // override render method
-export { render };
+export { render, renderHook };
