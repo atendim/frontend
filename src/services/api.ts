5ed47ade1d62f1aa52d535f1d7af583165ad0379
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2023-02-04 00:31:40 -0300

    feat: change api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000"
+  baseURL: "https://backend-hzz8ceb38-atendim-server.vercel.app/"
 })
 
 api.interceptors.request.use(config => {
